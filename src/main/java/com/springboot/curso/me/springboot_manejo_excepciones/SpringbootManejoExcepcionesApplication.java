2f9953feacf9580551d2c7f12edf44f32fd14cd7
Alejandra C. Araya <Arayadiaz.ac@gmail.com>
2024-08-11 22:18:17 -0300

    commit inicial

---
+++ src/main/java/com/springboot/curso/me/springboot_manejo_excepciones/SpringbootManejoExcepcionesApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.curso.me.springboot_manejo_excepciones;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootManejoExcepcionesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootManejoExcepcionesApplication.class, args);
+	}
+
+}
